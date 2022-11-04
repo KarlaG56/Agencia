6e3f9fb962a023f2b5e72fbe52004f88e4569de3
KarlaG56 <213356@ids.upchiapas.edu.mx>
2022-11-04 09:38:29 -0600

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+
+import Login from "./Components/Page/Login";
+import Register from "./Components/Page/Register";
+import Home from "./Components/Page/Home";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Flight from "./Components/Page/CruiceShip";
 
 
 function App() {
     return (
 
-        <Flight/>
+        <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Login/>}/>
+                    <Route path='/Home' element={<Home/>}/>
+                    <Route path='/Register' element={<Register/>}/>
+                </Routes>
+        </BrowserRouter>
     )
 }
 
