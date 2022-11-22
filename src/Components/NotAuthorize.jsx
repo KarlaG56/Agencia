001c42aaf6ff58f9114a399e123c2738ff21fc81
KarlaG56 <213356@ids.upchiapas.edu.mx>
2022-11-21 22:51:33 -0600

    [update] not authorized

--- src/Components/NotAuthorize.jsx
+++ src/Components/NotAuthorize.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 function NotAuthorized(){
     return(
         <div className="container-NotPage">
-            <div className='Error'>
-                <img src="/img/member.png" id='Error-img'/>
-               <h1 id='Error-title'>ERROR 404</h1> 
-               <p id='Error-text'>Sorry, page not found.</p>
+            <div className='Error2'>
+                <img src="/img/member.png" id='Error-img2'/>
+               <h1 id='Error-title2'>ERROR 401</h1> 
+               <p id='Error-text2'>Sorry, but you do not have permission for this page.</p>
                <Link to="/" ><button className='button-Notpage'>Take home</button></Link> 
             </div>
         </div>
